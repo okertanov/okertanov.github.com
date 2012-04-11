1d851edfd16f9d6c4b65bd494726b3ca94f29507
okertanov <okertanov@gmail.com>
2012-04-11 11:40:06 +0300

    fix ready state

--- assets/lib/app/application.js
+++ assets/lib/app/application.js
@@ -2,8 +2,19 @@
 // Copyright (C) 2012 Oleg Kertanov <okertanov@gmail.com>
 //
 
+//
+// IIFE
+//
 (function()
 {
+
+// Strict mode by default
+"use strict";
+
+// jQuery-like Ready
+$(function() {
     $('table').addClass('table');
+});
+
 })();
 
